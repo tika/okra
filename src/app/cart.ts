4a4bc8702c71746978abe2f62b3cc5cf69a84195
tika <48658947+tika@users.noreply.github.com>
2022-08-01 13:29:16 +0100

    fix: cart

--- src/app/cart.ts
+++ src/app/cart.ts
@@ -25,8 +25,31 @@ export function addToCart(item: Item, amount: number) {
         amount,
     });
 
-    LocalStorage.setItem(cartKey, items);
+    // Compact duplicate ids
+    LocalStorage.setItem(cartKey, clean(items));
+
     return items;
+}
+
+// Cleans up where we have duplicate ids
+// Returns a new array
+function clean(x: CartItem[]) {
+    const y: CartItem[] = [];
+
+    for (let i = 0; i < x.length; i++) {
+        const elem = x[i];
+        const match = y.filter((it) => it.itemId === elem.itemId);
+
+        // If y doesn't include this itemId, then add
+        if (match.length === 0) {
+            y.push(elem);
+        } else {
+            // Otherwise find the amount
+            match[0].amount += elem.amount;
+        }
+    }
+
+    return y;
 }
 
 export function setCart(items: CartItem[]) {
